3dc9477982faabd76d30d2f29018752b38c5af96
grighq <grighq@gmail.com>
2026-04-21 12:00:31 +0300

    Add easy problem "Two Sum"

---
+++ easy/two_sum/two_sum.go
@@ -0,0 +1,12 @@
+package twosum
+
+func twoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+	for i, num := range nums {
+		if _, ok := hashMap[target-num]; ok {
+			return []int{hashMap[target-num], i}
+		}
+		hashMap[num] = i
+	}
+	return []int{}
+}
---
+++ easy/two_sum/two_sum_test.go
@@ -0,0 +1,49 @@
+package twosum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums     []int
+		target   int
+		expected []int
+	}{
+		{
+			name:     "Case:1",
+			nums:     []int{2, 7, 11, 15},
+			target:   9,
+			expected: []int{0, 1},
+		},
+		{
+			name:     "Case:2",
+			nums:     []int{3, 2, 4},
+			target:   6,
+			expected: []int{1, 2},
+		},
+		{
+			name:     "Case:3",
+			nums:     []int{3, 2, 4},
+			target:   6,
+			expected: []int{1, 2},
+		},
+		{
+			name:     "No answer",
+			nums:     []int{1, 2, 3},
+			target:   10,
+			expected: []int{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := twoSum(tt.nums, tt.target)
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("twoSum() = %v, want %v", result, tt.expected)
+			}
+		})
+	}
+}
