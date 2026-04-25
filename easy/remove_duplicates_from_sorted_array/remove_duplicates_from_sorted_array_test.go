055b4607141de78bda539136611650c541bc8696
grighq <grighq@gmail.com>
2026-04-25 09:48:22 +0300

    Rename test func

--- easy/remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array_test.go
+++ easy/remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRemoveDuplicatesFromSortedArray(t *testing.T) {
+func TestRemoveDuplicates(t *testing.T) {
 	assert.Equal(t, 1, removeDuplicates([]int{3}))
 	assert.Equal(t, 2, removeDuplicates([]int{1, 1, 2}))
 	assert.Equal(t, 5, removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}))
