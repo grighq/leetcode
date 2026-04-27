fb427da6d770b81d5a3a56ae04da008651e71ae8
grighq <grighq@gmail.com>
2026-04-27 18:08:51 +0300

    Add find_index_of_the_first_occurence_in_a_string solution with test

---
+++ easy/find_index_of_the_first_occurence_in_a_string/find_index_of_the_first_occurence_in_a_string.go
@@ -0,0 +1,10 @@
+package findindexofthefirstoccurenceinastring
+
+func strStr(haystack, needle string) int {
+	for i := 0; len(needle) <= len(haystack[i:]); i++ {
+		if needle == haystack[i:i+len(needle)] {
+			return i
+		}
+	}
+	return -1
+}
---
+++ easy/find_index_of_the_first_occurence_in_a_string/find_index_of_the_first_occurence_in_a_string_test.go
@@ -0,0 +1,14 @@
+package findindexofthefirstoccurenceinastring
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindIndexOfTheFirstOccurenceInAString(t *testing.T) {
+	assert.Equal(t, 0, strStr("s", "s"))
+	assert.Equal(t, 2, strStr("hell", "ll"))
+	assert.Equal(t, 0, strStr("sadbutsac", "sad"))
+	assert.Equal(t, -1, strStr("leetcode", "leeto"))
+}
