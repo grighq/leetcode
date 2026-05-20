f6e7c1fa04f2772c694fdb41445541f821b20b92
grighq <grighq@gmail.com>
2026-05-20 12:05:08 +0300

    Refactor Make two loops for easy reading and understaning

--- easy/merge_sorted_array/merge.go
+++ easy/merge_sorted_array/merge.go
@@ -1,18 +1,24 @@
 package mergesortedarray
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	p1 := m - 1
-	p2 := n - 1
-	for i := m + n - 1; i >= 0; i-- {
-		if p2 < 0 {
-			return
-		}
-		if p1 >= 0 && nums1[p1] > nums2[p2] {
-			nums1[i] = nums1[p1]
+
+	p := m + n - 1
+	p1, p2 := m-1, n-1
+
+	for p1 >= 0 && p2 >= 0 {
+		if nums1[p1] > nums2[p2] {
+			nums1[p] = nums1[p1]
 			p1--
 		} else {
-			nums1[i] = nums2[p2]
+			nums1[p] = nums2[p2]
 			p2--
 		}
+		p--
+	}
+
+	for p2 >= 0 {
+		nums1[p] = nums2[p2]
+		p2--
+		p--
 	}
 }
