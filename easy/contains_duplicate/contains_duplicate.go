696285c0ac31efe33217784859f5be02f984ff67
grighq <grighq@gmail.com>
2026-05-27 18:29:11 +0300

    Add contains_duplicate solution with test

---
+++ easy/contains_duplicate/contains_duplicate.go
@@ -0,0 +1,13 @@
+package containsduplicate
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]struct{}, len(nums))
+	for _, num := range nums {
+		if _, ok := m[num]; ok {
+			return true
+		}
+		m[num] = struct{}{}
+	}
+
+	return false
+}
