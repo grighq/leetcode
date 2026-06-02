f903449631d79582c26e958ca1496f5b9078532e
grighq <grighq@gmail.com>
2026-06-02 11:25:56 +0300

    Add island_perimeter solution with test

---
+++ easy/island_perimeter/island_perimeter_test.go
@@ -0,0 +1,13 @@
+package islandperimeter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIslandPerimeter(t *testing.T) {
+	assert.Equal(t, 4, islandPerimeter([][]int{{1}}))
+	assert.Equal(t, 4, islandPerimeter([][]int{{1, 0}}))
+	assert.Equal(t, 16, islandPerimeter([][]int{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}))
+}
