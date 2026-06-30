f35ccb2660f43197023202d22b69cf67926e2781
grighq <grighq@gmail.com>
2026-06-30 13:10:42 +0300

    Add binary_search solution with test

---
+++ easy/binary_search/binary_search_test.go
@@ -0,0 +1,12 @@
+package binarysearch
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearch(t *testing.T) {
+	assert.Equal(t, 4, search([]int{-1, 0, 3, 5, 9, 12}, 9))
+	assert.Equal(t, -1, search([]int{-1, 0, 3, 5, 9, 12}, 2))
+}
