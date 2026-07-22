39dcdefe97b007cf58d72df468f1998b7890dde4
grighq <grighq@gmail.com>
2026-07-22 12:56:38 +0300

    Add additional solution for n_repeated_element_in_size_2n_array

--- easy/n_repeated_element_in_size_2n_array/n_repeated_element_in_size_2n_array.go
+++ easy/n_repeated_element_in_size_2n_array/n_repeated_element_in_size_2n_array.go
@@ -13,3 +13,13 @@ func repeatedNTimes(nums []int) int {
 
 	return res
 }
+
+// func repeatedNTimes(nums []int) int {
+// 	for i := 2; i < len(nums); i++ {
+// 		if nums[i] == nums[i-1] || nums[i] == nums[i-2] {
+// 			return nums[i]
+// 		}
+// 	}
+//
+// 	return nums[0]
+// }
